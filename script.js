9996f0af8be2d5afc2a59d8507b66e2c48019345
Kaai Suzuki <kaaisuzuki@Kaais-MacBook-Pro.local>
2019-01-03 21:33:17 -0800

    add animation with executing script

--- script.js
+++ script.js
@@ -17,21 +17,34 @@ const sequence = [...contents, 'relax', ...contents.slice().reverse(), 'relax'];
 //get element to activate animation by clicking button
 const circle = document.querySelector('#circle');
 const button = document.querySelector('#animateButton');
-const endMessage = document.querySelector('#endMessage');
+// const endMessage = document.querySelector('#endMessage');
+
+//finish and appear endMessage when button has been clicked again
+const finishExercise = () =>{
+  // button.removeAttribute('disabled');
+  document.querySelector('#endMessage').innerHTML = "Well done.";
+  button.innerHTML = "Start Whenever You're Ready";
+}
 
 const animate = (item, index) => {
   setTimeout(() => {
     circle.innerText = item;
+    if (item === 'relax') {
+      circle.className = 'circle';
+    } else {
+      circle.classList.add('circle-big', 'circle-' + item);
+    }
     if (index === sequence.length - 1) {
       //make sure the animation completes before enabling button and adding text
-      setTimeout(finishExercise, 2500);
+      setTimeout(finishExercise, 1500);
     }
   }, 5000 * index);
 }
 
 //create animation for when clicking buttons
 const animateAll = () => {
-  button.setAttribute('disabled', true);
+  // button.setAttribute('disabled', true);
+  button.innerHTML = "Finish Exercise";
   endMessage.innerHTML = '';
   sequence.forEach((item, index) => {
     animate(item, index);
